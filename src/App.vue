dfe63838254917e27d6fd1db73e1fa72678df978
gihooh <carloginocatapang@gmail.com>
2018-04-06 18:08:57 +0800

    created new project.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <div id="app">
-    <h1>Hello {{pogi}}</h1>
+    <img src="./assets/logo.png">
+    <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data: {
-    pogi : "Carlo Gino Catapang"
-  }
+  name: 'App'
 }
 </script>
 
